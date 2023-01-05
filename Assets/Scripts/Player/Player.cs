70a85d13d1c19181e5edfcc41b3df31382de2037
Lucas Albuquerque <guinho.lucas7@gmail.com>
2023-01-04 22:07:29 -0300

    add rotation for player
    quando clico com as teclasw/s ou com as setas rigth/left , o player gira

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,9 +6,11 @@ public class Player : MonoBehaviour
 {
     public CharacterController characterController;
     public float speed;
+    public float spinSpeed;
 
     void Update()
     {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * spinSpeed * Time.deltaTime, 0);
         var verticalSpeed = transform.forward * Input.GetAxis("Vertical") * speed;
 
         characterController.Move(verticalSpeed * Time.deltaTime);
