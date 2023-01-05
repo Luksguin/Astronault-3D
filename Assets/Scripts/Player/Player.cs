1c73fd7960857565fc2bb0d45459acb89ce0357d
Lucas Albuquerque <guinho.lucas7@gmail.com>
2023-01-05 02:42:37 -0300

    add run
    se a tecla shift estiver segurada, a velocidade do player dobra

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,9 +18,12 @@ public class Player : MonoBehaviour
     public string runBool;
     public string jumpBool;
 
-    private void Moviments()
+    private void Run()
     {
         transform.Rotate(0, Input.GetAxis("Horizontal") * spinSpeed * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) speed *= 2;
+        if (Input.GetKeyUp(KeyCode.LeftShift)) speed /= 2;
         
         var InputVertical = Input.GetAxis("Vertical");
         var run = transform.forward * InputVertical * speed;
@@ -29,6 +32,18 @@ public class Player : MonoBehaviour
         run.y = _vGravity;
         characterController.Move(run * Time.deltaTime);
 
+        if (InputVertical != 0)
+        {
+            playerAnimator.SetBool(runBool, true);
+        }
+        else 
+        {
+            playerAnimator.SetBool(runBool, false);
+        }
+    }
+
+    private void Jump()
+    {
         if (characterController.isGrounded)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -41,19 +56,11 @@ public class Player : MonoBehaviour
                 playerAnimator.SetBool(jumpBool, false);
             }
         }
-
-        if (InputVertical != 0)
-        {
-            playerAnimator.SetBool(runBool, true);
-        }
-        else 
-        {
-            playerAnimator.SetBool(runBool, false);
-        }
     }
 
     void Update()
     {
-        Moviments();
+        Run();
+        Jump();
     }
 }
