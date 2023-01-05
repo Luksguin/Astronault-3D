ad4772c338dc78e1aa36f530c9f1d4a0a7125fcb
Lucas Albuquerque <guinho.lucas7@gmail.com>
2023-01-04 22:16:39 -0300

    add gravity
    o player não flutua mais e cai no cenário

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,11 +7,16 @@ public class Player : MonoBehaviour
     public CharacterController characterController;
     public float speed;
     public float spinSpeed;
+    public float gravity;
+
+    private float _vGravity;
 
     void Update()
     {
         transform.Rotate(0, Input.GetAxis("Horizontal") * spinSpeed * Time.deltaTime, 0);
         var verticalSpeed = transform.forward * Input.GetAxis("Vertical") * speed;
+
+        verticalSpeed.y -= gravity;
 
         characterController.Move(verticalSpeed * Time.deltaTime);
     }
