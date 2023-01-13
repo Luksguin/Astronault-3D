35534664ae186dd4e0faa7ac0f186696bd149131
Lucas Albuquerque <guinho.lucas7@gmail.com>
2023-01-13 17:56:15 -0300

    create new types of projectiles
    cada arma possui um tipo especifico de projétil com dano, velocidade e tempo de destruição diferentes

--- Assets/Scripts/Gun/GunBase.cs
+++ Assets/Scripts/Gun/GunBase.cs
@@ -6,13 +6,13 @@ public class GunBase : MonoBehaviour
 {
     public ProjectilleBase projectillePrefab;
     public Transform positionShoot;
-    public float timeToDestroyProjectille;
+    //public float timeToDestroyProjectille;
     public float timeBetweenShoot;
 
     protected Coroutine _currentCoroutine;
     private void Update()
     {
-        projectillePrefab.timeToDestroy = timeToDestroyProjectille;
+        //projectillePrefab.timeToDestroy = timeToDestroyProjectille;
     }
 
     protected virtual IEnumerator StartShoot()
