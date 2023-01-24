a2ced189554a5493e7daa6f1d58e31ad6935cd92
Lucas Albuquerque <guinho.lucas7@gmail.com>
2023-01-24 02:07:52 -0300

    add check point base
    configurei o check point para quando o player entra nele, mudar de cor

---
+++ Assets/CheckPoint/CheckPointBase.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointBase : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public Color onColor;
+
+    public int key;
+    private string _keyString = "CheckPointKey";
+
+    private bool _isCheck = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player" && !_isCheck)
+        {
+            TurnOn();
+            SaveCheckPoint();
+        }
+    }
+
+    /*private void OnTriggerExit(Collider other)
+    {
+        TurnOff();
+    }*/
+
+    private void TurnOn()
+    {
+        meshRenderer.material.SetColor("_EmissionColor", onColor);
+        Debug.Log("On");
+    }
+
+    private void TurnOff()
+    {
+        meshRenderer.material.SetColor("_EmissionColor", Color.gray);
+    }
+
+    private void SaveCheckPoint()
+    {
+        if(PlayerPrefs.GetInt("CheckPointKey", 0) > key)
+        {
+            PlayerPrefs.SetInt("CheckPointKey", key);
+        }
+            _isCheck = true;
+    }
+}
